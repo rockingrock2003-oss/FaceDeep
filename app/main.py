4791283e2962b8f634d35dab413de35245e48dec
Abdur Rahim <raheemabdur203@gmail.com>
2026-09-13 13:32:29 +0530

    fix: CORS middleware ordering, allow all methods/headers

--- app/main.py
+++ app/main.py
@@ -43,8 +43,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_PREFIX)
